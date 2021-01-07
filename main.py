11b34dd93bb97e390f07b83a8a0b299d8ad4dd55
pradyX <pradyumnbhushan@gmail.com>
2021-01-07 11:39:08 +0530

    main.py: open zoom

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ import time
 import pandas as pd
 from datetime import datetime   
 
+def openZoom():
+    # uncomment for Windows
+    subprocess.Popen(r"C:\Users\prady\AppData\Roaming\Zoom\bin\Zoom.exe") # Change username here
+
+    # uncomment for Linux
+    #subprocess.call(["zoom"])
+
+    time.sleep(15)
+    
+openZoom()
